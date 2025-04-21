427b13a4d60ce72e8dd71f16e28e256d51e4e8c1
SergeyK92 <sergey.korshak.92@mail.ru>
2025-04-21 18:22:06 +0300

    Реализация Orders API

--- server_app/orders/models.py
+++ server_app/orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
+
 
 # Create your models here.
+class Order(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- server_app/orders/views.py
+++ server_app/orders/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Order
+from .serializers import OrderSerializer
+from users.models import User
+
 
 # Create your views here.
+class OrderListCreateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def create(self, request, *args, **kwargs):
+        user_id = request.data.get('user')
+        if not User.objects.filter(id=user_id).exists():
+            return Response(
+                {"error": "User does not exist."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return super().create(request, *args, **kwargs)
